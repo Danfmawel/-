df7794dc003b33330baa78f9bf869acd9b8223ee
Danfmawel <153481892+Danfmawel@users.noreply.github.com>
2024-12-20 18:47:22 +0500

    /

---
+++ client/src/app/auth/register/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next'
+
+import { RegisterForm } from '@/features/auth/components'
+
+/*import { RegisterForm } from '@/features/auth/components'
+<RegisterForm />
+*/
+
+export const metadata: Metadata = {
+	title: 'Создать аккаунт'
+}
+
+export default function RegisterPage() {
+	return <RegisterForm />
+}
